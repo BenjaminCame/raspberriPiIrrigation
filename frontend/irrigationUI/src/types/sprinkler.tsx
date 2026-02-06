3073cf5ac5f844b4e6d6dbaba518f2bf6e8249b4
Benjamin Came <38061513+BenjaminCame@users.noreply.github.com>
2026-02-06 13:14:33 +1030

    created docker file and compose yml for server

--- frontend/irrigationUI/src/types/sprinkler.tsx
+++ frontend/irrigationUI/src/types/sprinkler.tsx
@@ -1,13 +1,14 @@
 export type SprinklerZone = {
-    id: number,
+    id: string,
     name: string,
     isActive: boolean,
     gpioPin: number
 }
 
+//TODO: I now have a json file in the server hat holds this, will need to implement data pasing on init 
 export const SPRINKLER_ZONES: SprinklerZone[] = [
-    { id: 1, name: 'Front Lawn', gpioPin: 17, isActive: false },
-    { id: 2, name: 'Back Yard', gpioPin: 18, isActive: false },
-    { id: 3, name: 'Side Yard', gpioPin: 27, isActive: false },
-    { id: 4, name: 'Back Garden Bed', gpioPin: 26, isActive: false },
+    { id: 'frontLawn', name: 'Front Lawn', gpioPin: 17, isActive: false },
+    { id: 'backYard', name: 'Back Yard', gpioPin: 18, isActive: false },
+    { id: 'sideYard', name: 'Side Yard', gpioPin: 27, isActive: false },
+    { id: 'backGarden', name: 'Back Garden Bed', gpioPin: 26, isActive: false },
 ]
