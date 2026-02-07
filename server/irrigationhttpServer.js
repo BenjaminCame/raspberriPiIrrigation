966dca0c7ec5e7168240e89d98022967c1be1fd7
Benjamin Came <38061513+BenjaminCame@users.noreply.github.com>
2026-02-07 16:54:26 +1030

    http status system

--- server/irrigationhttpServer.js
+++ server/irrigationhttpServer.js
@@ -1,7 +1,18 @@
 const http = require('node:http');
+const json = require('./irrigators.json') //TODO: this will probably not be the most recent status of the app
+
 
 const server = http.createServer((req,res) => {
     const { url, method } = req;
+
+    if ( method === 'GET' & url === '/status' ) {
+        res.end(JSON.stringify(json))
+    }
+    else if ( method === 'PUT'){
+        if ( url === '/on' ){
+            
+        }
+    }
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(`req url = ${url}, method = ${method} \n`);
