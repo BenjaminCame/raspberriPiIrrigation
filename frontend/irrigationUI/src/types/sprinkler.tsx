c2e3127a8a34427a1440cc72c1c954e5ae4844d0
Benjamin Came <38061513+BenjaminCame@users.noreply.github.com>
2026-02-10 21:18:21 +1030

    cleanup

--- frontend/irrigationUI/src/types/sprinkler.tsx
+++ frontend/irrigationUI/src/types/sprinkler.tsx
@@ -4,11 +4,3 @@ export type SprinklerStatus = {
     pin: number,
     isActive: boolean
 }
-
-//TODO: I now have a json file in the server that holds this, will need to implement data passing on init 
-// export const SPRINKLER_ZONES: SprinklerZone[] = [
-//     { id: 'frontLawn', name: 'Front Lawn', gpioPin: 17, isActive: false },
-//     { id: 'backYard', name: 'Back Yard', gpioPin: 18, isActive: false },
-//     { id: 'sideYard', name: 'Side Yard', gpioPin: 27, isActive: false },
-//     { id: 'backGarden', name: 'Back Garden Bed', gpioPin: 26, isActive: false },
-// ]
